e89f83264794fd581dd297a36a5bf74a459bfa36
Petra-78 <partos.petra.0305@gmail.com>
2025-11-20 16:45:53 +0100

    Move messages array to it's own file

---
+++ data.js
@@ -0,0 +1,14 @@
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    date: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    date: new Date(),
+  },
+];
+
+module.exports = messages;
