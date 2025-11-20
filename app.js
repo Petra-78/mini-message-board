fb6fea700a07e399d9072f8b6891ea0853079e81
Petra-78 <partos.petra.0305@gmail.com>
2025-11-20 16:48:15 +0100

    Add routes

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const newRouter = require("./routes/newRouter");
+const indexRouter = require("./routes/indexRouter");
+
+app.use(express.urlencoded({ extended: true }));
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-  res.send("hello world");
-});
+app.use("/", indexRouter);
+
+app.use("/new", newRouter);
 
 const PORT = 3000;
 app.listen(PORT, () => {
