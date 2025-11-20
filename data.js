bbf900ea9107234e8168d77c7d665700f9c9db67
Petra-78 <partos.petra.0305@gmail.com>
2025-11-20 17:27:39 +0100

    Format date structure

--- data.js
+++ data.js
@@ -2,12 +2,24 @@ const messages = [
   {
     text: "Hi there!",
     user: "Amando",
-    date: new Date(),
+    date: new Date().toLocaleString("en-GB", {
+      day: "2-digit",
+      month: "short", // "Feb"
+      year: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+    }),
   },
   {
     text: "Hello World!",
     user: "Charles",
-    date: new Date(),
+    date: new Date().toLocaleString("en-GB", {
+      day: "2-digit",
+      month: "short",
+      year: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+    }),
   },
 ];
 
--- routes/newRouter.js
+++ routes/newRouter.js
@@ -9,7 +9,17 @@ router.get("/", (req, res) => {
 router.post("/", (req, res) => {
   const userName = req.body.userName;
   const message = req.body.message;
-  messages.push({ text: message, user: userName, date: new Date() });
+  messages.push({
+    text: message,
+    user: userName,
+    date: new Date().toLocaleString("en-GB", {
+      day: "2-digit",
+      month: "short",
+      year: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+    }),
+  });
   res.redirect("/");
 });
 
