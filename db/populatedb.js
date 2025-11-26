b434277c24bc19913d49a4965f5754216713de56
Petra-78 <partos.petra.0305@gmail.com>
2025-11-26 17:50:30 +0100

    Add ssl

--- db/populatedb.js
+++ db/populatedb.js
@@ -35,6 +35,9 @@ async function main() {
   console.log("seeding...");
   const client = new Client({
     connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
   });
   await client.connect();
   await client.query(SQL);
