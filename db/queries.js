916cf50266631cec55e517e9cf585781c8c06deb
Petra-78 <partos.petra.0305@gmail.com>
2025-11-26 14:43:33 +0100

    Add message adding function

--- db/queries.js
+++ db/queries.js
@@ -9,7 +9,19 @@ async function addMessage(username, message, date) {
 
 async function getAllMessages(req, res) {
   const { rows } = await pool.query("SELECT * FROM messages");
-  return rows;
+  const formattedMessages = rows.map((msg) => {
+    return {
+      ...msg,
+      date: new Date(msg.date).toLocaleString("en-GB", {
+        day: "2-digit",
+        month: "short",
+        year: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+      }),
+    };
+  });
+  return formattedMessages;
 }
 
 module.exports = {
--- routes/newRouter.js
+++ routes/newRouter.js
@@ -1,25 +1,17 @@
 const { Router } = require("express");
 const router = Router();
-const messages = require("../data");
+const db = require("../db/queries");
 
 router.get("/", (req, res) => {
   res.render("form");
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   const userName = req.body.userName;
   const message = req.body.message;
-  messages.push({
-    text: message,
-    user: userName,
-    date: new Date().toLocaleString("en-GB", {
-      day: "2-digit",
-      month: "short",
-      year: "numeric",
-      hour: "2-digit",
-      minute: "2-digit",
-    }),
-  });
+  const date = new Date();
+
+  const messages = db.addMessage(userName, message, date);
   res.redirect("/");
 });
 
