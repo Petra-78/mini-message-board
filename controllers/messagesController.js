dcb470c1e4298fd39127a8c5eb3eb167d99ebb2b
Petra-78 <partos.petra.0305@gmail.com>
2025-11-26 16:38:34 +0100

    Add express validator

--- controllers/messagesController.js
+++ controllers/messagesController.js
@@ -1,16 +1,45 @@
 const db = require("../db/queries");
+const { body, validationResult, matchedData } = require("express-validator");
 
-async function postUploadMessages(req, res) {
-  const userName = req.body.userName;
-  const message = req.body.message;
-  const date = new Date();
+const validateMessage = [
+  body("username")
+    .trim()
+    .escape()
+    .isLength({ min: 2, max: 20 })
+    .withMessage(`Username must be 2-20 characters long`),
+  body("message")
+    .trim()
+    .escape()
+    .isLength({ min: 1, max: 300 })
+    .withMessage("Message must be 1-300 characters long"),
+];
 
-  const messages = db.addMessage(userName, message, date);
-  res.redirect("/");
-}
+const postUploadMessages = [
+  validateMessage,
+  (req, res) => {
+    const errors = validationResult(req);
+    const inputData = req.body;
+    console.log(inputData);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).render("form", {
+        errors: errors.array(),
+        inputData,
+      });
+    }
+    const { username, message } = matchedData(req);
+    const date = new Date();
+
+    db.addMessage(username, message, date);
+    res.redirect("/");
+  },
+];
 
 function renderForm(req, res) {
-  res.render("form");
+  res.render("form", {
+    errors: [],
+    inputData: {},
+  });
 }
 
 async function renderMessages(req, res) {
