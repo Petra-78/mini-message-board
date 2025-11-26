ad05a3133f53667fa6f102c33858430a97894e98
Petra-78 <partos.petra.0305@gmail.com>
2025-11-26 19:27:17 +0100

    Change ssl config so it accepts both local and public connection

--- db/pool.js
+++ db/pool.js
@@ -2,7 +2,7 @@ const { Pool } = require("pg");
 
 module.exports = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  ssl: process.env.DATABASE_URL.includes("railway.app")
+    ? { rejectUnauthorized: false }
+    : false,
 });
